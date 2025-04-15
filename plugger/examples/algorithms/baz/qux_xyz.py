cd01b7e56d8e5404cd72073b43499e7438cb4df7
rmg <rmg@mail.com>
2025-04-16 01:23:53 +0200

    CI: run hard-check

--- plugger/examples/algorithms/baz/qux_xyz.py
+++ plugger/examples/algorithms/baz/qux_xyz.py
@@ -4,7 +4,7 @@ from plugger.core.base import BaseAlgorithm, Result
 
 
 class QuxXyz(BaseAlgorithm):
-    async def __call__(self, *args, **kwargs) -> Iterable[Result]:
+    async def __call__(self, *args, **kwargs) -> Iterable[Result]:  # type: ignore
         if kwargs.get("arg"):
             return [Result(True)]
 
